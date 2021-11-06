ca3cd77cb9c3a0430366e514f08f44ff0fc06c1f
AndreyKryukov <ander.kryukov@yandex.ru>
2021-11-06 14:34:56 +0300

    feat: added trim method

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,5 +5,5 @@ const wayReading = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(wayReading, 'utf-8');
 
 stream.on('data', (el) => {
-  console.log(el);
+  console.log(el.trim());
 });
